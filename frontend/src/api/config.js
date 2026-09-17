ba37935616a899df5981c75d7659b842f7e9d52e
Krishna Rathod <krishna.rathod227@gmail.com>
2026-09-17 17:45:13 +0530

    fix: config.js

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = import.meta.env.VITE_API_URL || "";
+VITE_API_URL="https://usermanagement-15.onrender.com"
---
+++ src/api/config.js
@@ -0,0 +1,7 @@
+<<<<<<< HEAD:frontend/src/api/config.js
+export const API_BASE_URL = import.meta.env.VITE_API_URL || "";
+=======
+export const API_BASE_URL = import.meta.env.VITE_API_URL || "";
+
+console.log("API_BASE_URL:", API_BASE_URL);
+>>>>>>> b515c53 (fix: config.js):src/api/config.js
